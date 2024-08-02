84e0834a249f0969bdf1f12f07d814995ee82add
iiiiin <keepa0041@naver.com>
2024-08-02 17:37:59 +0900

    Tutorial_Quickstart

---
+++ mutorial/quickstart/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+
+from quickstart.serializers import Groupserializer, UserSerializer
+
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = Groupserializer
+    permission_classes = [permissions.IsAuthenticated]
+    

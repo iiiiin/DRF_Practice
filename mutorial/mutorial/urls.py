493c1bd7e6292c233e660e2150ddf5651af3abf8
iiiiin <keepa0041@naver.com>
2024-08-04 13:21:00 +0900

    Tutorial_Serialization

--- mutorial/mutorial/urls.py
+++ mutorial/mutorial/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for mutorial project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import include, path
 from rest_framework import routers
 
@@ -26,6 +9,7 @@ router.register(r'groups',views.GroupViewSet)
 
 
 urlpatterns = [
-    path('',include(router.urls)),
+    # path('',include(router.urls)),
+    path('',include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))        
 ]
